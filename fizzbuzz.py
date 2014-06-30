5da55a78eeb6b6054607e26d0ac8730f48d2ac9f
nbcesar <nbcesar@gmail.com>
2014-06-30 12:38:24 -0400

    included sys.argv

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -1,10 +1,14 @@
 #Fizz Buzz v2
+import sys
 
 print "Fizz buzz counting up to any number"
-   
+
 while True:
     try:
-        max_num = raw_input('How high should I go: ') 
+        if len(sys.argv) > 1:
+            max_num = sys.argv[1]
+        else:
+            max_num = raw_input('How high should I go: ')
         number = int(max_num)
         break
     except ValueError:
@@ -19,3 +23,15 @@ for n in range(1,int(max_num) + 1):
         print "fizz"
     else:
         print n
+
+
+'''def subtractor(a, b): 
+   """I subtract b from a and return the result"""  
+   print "I'm a function. My name is {}".format(subtractor.__name__)
+   print "I'm about to subtract {} and {}\n\n".format(a,b)
+   return a - b  # i output a value by using the return statement
+
+
+if __name__ == '__main__':
+    subtractor(3, 2)
+'''
